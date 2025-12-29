c5b13533ca859fbb8e9f2a56c2431d80dc093c74
Jakson <jakson.mesquita@hotmail.com>
2025-12-28 21:21:25 -0300

    mudando texto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -179,7 +179,7 @@ function AppContent() {
         />
       ) : (
         <div className="empty-state">
-          <h3>Nenhuma tarefa por aqui 📝</h3>
+          <h3>Nenhuma tarefa por aqui nesse momento📝</h3>
           <p>Adicione sua primeira tarefa usando o formulário acima!</p>
         </div>
       )}
